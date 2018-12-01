d6dc45dd9dad4fc408f8c4258657aaf7a8be211c
Student <class@vyatsu.ru>
2018-12-01 11:28:50 +0300

    Были добавлены новые функции

--- ConsoleBattleshipApp/Battleship_test.h
+++ ConsoleBattleshipApp/Battleship_test.h
@@ -4,7 +4,12 @@ class Battleship {
 private:
 	size_t width;
 	size_t height;
+
 public: 
 	Battleship();
 	~Battleship();
+
+	void run();
+	size_t getwidth();
+	size_t getheight();
 };
--- ConsoleBattleshipApp/main.cpp
+++ ConsoleBattleshipApp/main.cpp
@@ -1,6 +1,9 @@
 #include "libs.h"
+#include "Battleship_test.h"
 
 int main()
 {
+	Battleship game = Battleship();
+	game.run();
 	return 0;
 }
